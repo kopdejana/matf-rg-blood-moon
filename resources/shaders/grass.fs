3929d761fc4ded003a2caad2329139fa99c547cd
kopdejana <kopdejanacs@gmail.com>
2023-03-31 22:08:09 +0200

    grass shader modified

--- resources/shaders/grass.fs
+++ resources/shaders/grass.fs
@@ -6,7 +6,7 @@ in vec2 TexCoords;
 uniform sampler2D texture1;
 
 void main() {
-    vec4 texColor = texture(texture1, TexCoords);
+    vec4 texColor = texture(texture1, TexCoords) * 0.3f;
     if(texColor.a < 0.1)
         discard;
     FragColor = texColor;
